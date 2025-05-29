5e76529f7000ab73b7d80c212b737fa48fa3a276
Jared Tweed <jaredtwe@gmail.com>
2025-05-29 11:54:53 -0700

    app fix

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-// A very basic service worker that caches the app shell
 const CACHE_NAME = 'wedding-gallery-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+const ASSETS = [
+  '/', '/index.html', '/manifest.json',
   '/icons/image.png',
 ];
 
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
+  evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(cached =>
-      cached || fetch(evt.request)
-    )
+    caches.match(evt.request).then(cached => cached || fetch(evt.request))
   );
 });
