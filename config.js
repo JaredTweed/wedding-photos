edf85921e6689fc29141494daa19263ffb77ddbd
JaredTweed <jaredtwe@gmail.com>
2025-07-14 11:39:26 -0700

    made config file

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+export const SITE_TITLE       = 'Wedding Photos';
+export const PRIMARY_COLOR    = 'hsl(96 23.7% 54%)';
+
+export const REGION           = 'us-west-2';
+export const ID_POOL          = 'us-west-2:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+export const BUCKET           = 'your-s3-bucket-name';
+
+export const ENTRY_IMAGE      = './icons/original-photo.webp';
+export const ICO_IMAGE        = './icons/image.ico';
+
+
